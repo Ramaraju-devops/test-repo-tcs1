728c9c34d6ee0cabb231ce5bad4081c28a03860d
ramu <rraj95155@gmail.com>
2025-12-03 10:35:02 +0530

    test tf code 1

---
+++ terraform-script/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_resource_group" "abctestrg1" {
+  name     = "test-rg-gh1"
+  location = "West Europe"
+}
+
+
+resource "azurerm_resource_group" "abctestrg2" {
+  name     = "test-rg-gh2"
+  location = "West Europe"
+}
