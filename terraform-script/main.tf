9a8a931fc62547c08bac694baa64c2295db37387
ramu <rraj95155@gmail.com>
2025-12-09 10:29:36 +0530

    action and terraform code 1

--- terraform-script/main.tf
+++ terraform-script/main.tf
@@ -1,10 +1,22 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.55.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+
+
 resource "azurerm_resource_group" "abctestrg1" {
   name     = "test-rg-gh1"
   location = "West Europe"
 }
 
 
-resource "azurerm_resource_group" "abctestrg2" {
-  name     = "test-rg-gh2"
-  location = "West Europe"
-}
